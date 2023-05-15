a155c4893a56adbcf332e353d74f8d9decbcf0b5
taqinur <taqinur@biznovelty.com>
2023-05-15 15:39:54 +0600

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipesComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
